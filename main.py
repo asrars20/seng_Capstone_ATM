df962d8b4239a3b68c0d4013a82a374874e6e40d
asrars20 <97586601+asrars20@users.noreply.github.com>
2022-04-20 13:04:58 -0400

    test commit

--- main.py
+++ main.py
@@ -1,160 +1,8 @@
-<<<<<<< Updated upstream
-import datetime  # import date module
 
-
-# help('FORMATTING')
-
-# Dummy Data Dictionary
-# TODO: Create an "Account-Holders" Dictionary
-# TODO: Place User dictionaries inside Account-Holders dictionary
-# These dictionaries will be used to Create_Read_Update_Delete -CRUD- User's data on each view.
-
-# User Class not in use yet.
-# We will be using this class to CREATE user accounts and add them to our Dictionary of Users.
-class User:
-    # Initializing & assigning the given parameters to class attributes.
-    def __init__(self, pin, first, last, dob, cash, age, debit, saving, credit, credit_used, address,
-                 debit_transactions, savings_transactions, credit_transactions):
-        self.pin = pin
-        self.first = first
-        self.last = last
-        self.dob = dob
-        self.cash = cash
-        self.age = age
-        self.debit = debit
-        self.saving = saving
-        self.credit = credit
-        self.credit_used = credit_used
-        self.address = address
-        self.debit_transactions = debit_transactions
-        self.savings_transactions = savings_transactions
-        self.credit_transactions = credit_transactions
-
-
-# Reference to dummy user account that lacks parent dictionary.
-user_1 = {
-    'pin': 0000,
-    'first': 'Matulu',
-    'last': 'Shakur',
-    'dob': '00/00/00',
-    'cash': 1386,
-    'debit': 3218,
-    'saving': 247,
-    'credit': 900,
-    'credit_used': 113,
-    'address': '1884 ECU Blvd.',
-    'state': '1884 ECU Blvd.',
-    'debit_transactions': [],
-    'savings_transactions': [],
-    'credit_transactions': []
-}
-
-# Today's Date using function from 'datetime' module
-date = str(datetime.date.today())
-
-# Display user-name & date
-print(f"\n\n{user_1['last']:8s}{user_1['first']:14s} {date:8s}")
-# Display Divider
-print('-' * 35)
-
-# USER COMMAND LINE
-print(f'{"0 : Checking":20} {"1 : Credit":16s}')
-print(f'{"2 : Saving":20s} {"3 : Profile":16s}')
-print(f'{"4 : Exit":20s}\n')
-
-# Prompt User for Command-Line selection.
-user_input = input('Enter your selection:\n').strip().lower()
-# Add whitespace to separate views/screens/pages
-print('\n\n\n\n\n')
-
-# If the User input = EXIT, end program.
-if user_input.lower() != 'exit':
-    if user_input == '0' or user_input.lower() == 'checking':
-        # Display user-name & date
-        print(f"{user_1['last']:8s}{user_1['first']:14s} {date:8s}")
-        # Display Divider
-        print('-' * 35)
-
-        # Display 'CHECKING' COMMAND-LINE OPTIONS
-        print(f'{"Balance":<7} :   $ {user_1["debit"]:<10}')
-        print(f'{"0 : Transactions":20s} {"1 : Deposit":16s}')
-        print(f'{"2 : Withdraw":20s} {"3 : Exit":16s}\n')
-
-        # PROMPT USER FOR 2ND COMMAND
-        user_input = input('Enter your selection:\n')
-
-    if user_input == '1' or user_input.lower() == 'deposit':
-        # Display user-name & date
-        print(f"{user_1['last']:8s}{user_1['first']:14s} {date:8s}")
-        # Display Divider
-        print('-' * 35)
-
-        # Display 'DEPOSIT' COMMAND-LINE OPTIONS
-        print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
-        print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
-        print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
-
-        # PROMPT USER FOR 2ND COMMAND
-        user_input = input('Enter your selection:\n')
-
-    if user_input == '2' or user_input.lower() == 'saving':
-        # Display user-name & date
-        print(f"{user_1['last']:8s}{user_1['first']:14s} {date:8s}")
-        # Display Divider
-        print('-' * 35)
-
-        # Display 'SAVING' COMMAND-LINE OPTIONS
-        print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
-        print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
-        print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
-        user_input = input('Enter your selection:\n')
-
-    if user_input == '3' or user_input.lower() == 'credit':
-        # Display user-name & date
-        print(f"{user_1['last']:8s}{user_1['first']:14s} {date:8s}")
-        # Display Divider
-        print('-' * 35)
-
-        # Display 'CREDIT' COMMAND-LINE OPTIONS
-        print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
-        print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
-        print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
-        user_input = input('Enter your selection:\n')
-
-    if user_input == '4' or user_input.lower() == 'profile':
-        # Display user-name & date
-        print(f"{user_1['last']:8s}{user_1['first']:14s} {date:8s}")
-        # Display Divider
-        print('-' * 35)
-
-        # Display 'CREDIT' COMMAND-LINE OPTIONS
-        print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
-        print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
-        print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
-        user_input = input('Enter your selection:\n')
-
-    if user_input == '5' or user_input.lower() == 'exit':
-        # Display user-name & date
-        print(f"{user_1['last']:8s}{user_1['first']:14s} {date:8s}")
-        # Display Divider
-        print('-' * 35)
-
-        # Display 'CREDIT' COMMAND-LINE OPTIONS
-        print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
-        print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
-        print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
-        user_input = input('Enter your selection:\n')
-
-print('\n\nThank you for using XYZ Bank. Have a great day!')
-"""
-print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
-print(f'{"2 : Make Withdraw":20s} {"3 : Option 4":16s}')
-print(f'{"4 : Edit Profile":20s} {"5 : Exit":16s}')
-"""
-=======
 import datetime
 from admin import run_admin_program
 from account_holders import accounts
+
 
 # help('FORMATTING')
 
@@ -165,83 +13,127 @@ def run_user_program(user):
     After the PIN is verified, this program will run.
     The User Object/Dictionary is passed into this function to be used for CRUD.
     """
-    print(user['pin'])
-    print(user)
-    print(user['pin'])
     date = str(datetime.date.today())
     # Display user-name & date
     print(f"\n\n{user['last']:8s}{user['first']:14s} {date:8s}")
     # Display Divider
     print('-' * 35)
 
+    # User Commands
     print(f'{"0 : Accounts":20} {"1 : Deposit":16s}')
-    print(f'{"2 : Transfer":20s} {"3 : Settings":16s}')
-    print(f'{"4 : Products":20s} {"5 : Exit":16s}\n')
+    print(f'{"2 : Transfer":20s} {"4 : Products":16s}')
+    print(f'{"5 : Exit":20s}\n')
 
+    # Ask User for a Command
     user_input = input('Enter your selection:\n').strip().lower()
     print('\n\n\n\n\n')
 
     if user_input != 'exit':
-        if user_input == '0' or user_input == 'accounts':
+
+        if user_input == '0' or user_input.lower() == 'accounts':
             # Display user-name & date
-            print(f'{user["last"]:8s} {user["first"]:14s} {date:8s}')
+            print(f'{user["last"]:8s}, {user["first"]:14s} {date:8s}')
             # Display Divider
             print('-' * 35)
 
+            # User Commands
             print(f'{"Current Balance":<15} : {user["debit"]:>10}\n')
-            print(f'{"1 : View Transactions":25s} {"3 : Credit":16s}')
-            print(f'{"4 : Profile":25s} {"5 : Exit":16s}\n')
-            user_input = input('Enter your selection:\n')
+            # print(f'{"1 : View Transactions":25s} {"3 : Credit":16s}') # REMOVE
+            print(f'{"1 : Checking":25s} {"2 : Saving":16s}')
+            print(f'{"3 : Profile":25s} {"4 : Exit":16s}\n')
 
-            # If the user chooses the 'EXIT' command, restart/re-run the program.
-            if user_input == '5' or user_input == 'exit':
+            # Ask User for Next Command
+            user_input = input('Enter your selection:\n')
+            # If the User chooses the 'EXIT' command, restart/re-run the program.
+            if user_input == '1' or user_input.lower() == 'checking':
+
+                # Print USER DEBIT TRANSACTIONS
+                print(f"{user['first']:^10}{user['last']:<50}")
+                print(f"{'Checking':^80}\n")
+                print(f"{'Date':^10} {'Debit/Credit':^16} {'Expense':^21} {'Amount':^20} {'Balance':^8}")
+                print('-' * 80)
+                for transaction_ in user['debit_transactions']:
+                    # print(transaction_)
+                    print(f"{transaction_['date']:15} {transaction_['deb_cred']:15} {transaction_['expense']:15} "
+                          f"{transaction_['amt']:15}" f"{transaction_['remaining']:15}")
+                print()
+
+            if user_input == '2' or user_input == 'saving':
+
+                # Print USER SAVING TRANSACTIONS
+                print(f"{user['first']:^10}{user['last']:<50}")
+                print(f"{'Savings':^80}\n")
+                print(f"{'Date':^10} {'Debit/Credit':^16} {'Expense':^21} {'Amount':^20} {'Balance':^8}")
+                print('-' * 80)
+                for transaction_ in user['savings_transactions']:
+                    # print(transaction_)
+                    print(f"{transaction_['date']:15} {transaction_['deb_cred']:15} {transaction_['expense']:15} "
+                          f"{transaction_['amt']:15}" f"{transaction_['remaining']:15}")
+                print()
+
+                # If the User chooses the 'EXIT' command, restart/re-run the program.
+            if user_input == '3' or user_input == 'profile':
+                # Restart/re-run the program to get back to Main Menu
+                run_user_program(accounts)
+
+            # If the User chooses the 'EXIT' command, restart/re-run the program.
+            if user_input == '4' or user_input == 'exit':
                 # Restart/re-run the program to get back to Main Menu
                 run_user_program(accounts)
 
         if user_input == '1' or user_input == 'deposit':
             # Display user-name & date
-            print(f"{user['last']:8s}{user['first']:14s} {date:8s}")
+            print(f"{user['last']:8s}, {user['first']:14s} {date:8s}")
             # Display Divider
             print('-' * 35)
 
+            # User Commands
             print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
             print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
             print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
+
+            # Ask User for Next Command
             user_input = input('Enter your selection:\n')
 
-            # If the user chooses the 'EXIT' command, restart/re-run the program.
+            # If the User chooses the 'EXIT' command, restart/re-run the program.
             if user_input == '5' or user_input == 'exit':
                 # Restart/re-run the program to get back to Main Menu
                 run_user_program(accounts)
 
         if user_input == '2' or user_input == 'transfer':
             # Display user-name & date
-            print(f"{user['last']:8s}{user['first']:14s} {date:8s}")
+            print(f"{user['last']:8s}, {user['first']:14s} {date:8s}")
             # Display Divider
             print('-' * 35)
 
+            # User Commands
             print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
             print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
             print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
+
+            # Ask User for Next Command
             user_input = input('Enter your selection:\n')
 
-            # If the user chooses the 'EXIT' command, restart/re-run the program.
+            # If the User chooses the 'EXIT' command, restart/re-run the program.
             if user_input == '5' or user_input == 'exit':
                 # Restart/re-run the program to get back to Main Menu
                 run_user_program(accounts)
 
         if user_input == '3' or user_input == 'settings':
             # Display user-name & date
-            print(f"{user['last']:8s}{user['first']:14s} {date:8s}")
+            print(f"{user['last']:8s}, {user['first']:14s} {date:8s}")
             # Display Divider
             print('-' * 35)
 
+            # User Commands
             print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
             print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
             print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
+
+            # Ask User for Next Command
             user_input = input('Enter your selection:\n')
 
-            # If the user chooses the 'EXIT' command, restart/re-run the program.
+            # If the User chooses the 'EXIT' command, restart/re-run the program.
             if user_input == '5' or user_input == 'exit':
                 # Restart/re-run the program to get back to Main Menu
                 run_user_program(accounts)
@@ -250,30 +142,47 @@ def run_user_program(user):
             # Display Divider
             print('-' * 35)
 
-            print(f'{"Savings Interest :"}')
-            print(f'{"CD Interest :"}')
-            print(f'{"Business Loan Rates :"}\n')
+            print(f'{"Checking Interest :":<20} {"0.25% APY":>20}')
+            print(f'{"Savings Interest :":<20} {"0.050% APY":>20}')
+            print(f'{"Business Loan :":<20} {"3.89% APR":>20}')
+
+            print(f'{"Mortgage :":<20} {"3.500% APR":>20}')
+            print(f'{"Credit Card :":<20} {"9.99% APR":>20}')
+            print(f'{"Personal Loan :":<20} {"3.16% APR":>20}')
+            print(f'{"CD Interest :":<20} {"2.96% APR":>20}')
+
+            print(f'{"Auto Loan :":<20} {"4.093% APR":>20}')
+            print(f'{"Private Student Loan :":<20} {"3.16% APR":>18}\n')
 
             # Return Command
             print(f'{"0 : Return to Main Menu":20}\n')
-            # Ask Admin for Next Command
+
+            # Ask User for Next Command
             user_input = input('Enter your selection:\n')
 
-            # If the user chooses the 'EXIT' command, restart/re-run the program.
+            # If the User  chooses the 'EXIT' command, restart/re-run the program.
             if user_input == '0' or user_input == 'exit':
                 # Restart/re-run the program to get back to Main Menu
                 print('\n\n\n\n\n')
                 # start_atm()
-                run_admin_program(accounts)
-    print('Thank you for using XYZ Bank. Have a great day!')
+                run_user_program(accounts)
+    print("****************************************************************************")
+    print("*                                                                          *")
+    print("*                   Thank you for using College Bank ATM!                  *")
+    print("*                                                                          *")
+    print("****************************************************************************")
 
 
 def start_atm():
     # WELCOME MESSAGE (START OF PROGRAM)
-    print(f'{"Welcome to College Bank":^50s}\n\n')
+    print("****************************************************************************")
+    print("*                                                                          *")
+    print("*                   Welcome to College Bank ATM!                           *")
+    print("*                                                                          *")
+    print("****************************************************************************")
 
     # Request User-Account PIN
-    entered_pin = (input('Enter Your PIN: ')).strip()
+    entered_pin = (input('Please Enter Your PIN Below to Get Started: \n')).strip()
 
     # Check entered PIN against all Account-PIN's by looping
     for account in accounts.values():
@@ -285,6 +194,43 @@ def start_atm():
             # print(account)
             # Run user program/function
             run_user_program(account)
+        elif int(entered_pin) == 0000:
+            # Run admin program/function
+            run_admin_program(accounts)
+        elif int(entered_pin) != 0000 or int(entered_pin) != int(pin_ref):
+            print("****************************************************************************")
+            print("*                                                                          *")
+            print("*                   Sorry invalid PIN :(                                   *")
+            print("*               Do you want to try again? y/n                              *")
+            print("*                                                                          *")
+            print("****************************************************************************")
+            person_answer = input('y/n    ')
+            if person_answer =='y':
+                print("****************************************************************************")
+                print("*                                                                          *")
+                print("*                   Welcome to College Bank ATM!                           *")
+                print("*                                                                          *")
+                print("****************************************************************************")
+                entered_pin = (input('Please Enter Your PIN Below to Get Started: \n')).strip()
+                for account in accounts.values():
+                    # print(account['pin'])
+                    pin_ref = account['pin']
+
+                    # If PIN's match, run User_Program(). Passing the individual account data of matching PIN.
+                    if int(pin_ref) == int(entered_pin):
+                        # print(account)
+                        # Run user program/function
+                        run_user_program(account)
+                    elif int(entered_pin) == 0000:
+                        # Run admin program/function
+                        run_admin_program(accounts)
+            else:
+                print("****************************************************************************")
+                print("*                                                                          *")
+                print("*                   Thank you for using College Bank ATM!                  *")
+                print("*                                                                          *")
+                print("****************************************************************************")
+                break
 
             # TODO: COME UP W/ A WAY TO CLOSE PROGRAM ON COMMAND (EXIT FUNCTION)
 
@@ -297,4 +243,3 @@ def start_atm():
 
 
 start_atm()
->>>>>>> Stashed changes
